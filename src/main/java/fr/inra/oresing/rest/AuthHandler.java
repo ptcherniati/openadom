945a77a5f9cc0b2bac73edb540644b5241fe7df2
Benjamin <poussin@codelutin.com>
2019-02-25 09:31:46 +0100

    suppression d'import inutile

--- src/main/java/fr/inra/oresing/rest/AuthHandler.java
+++ src/main/java/fr/inra/oresing/rest/AuthHandler.java
@@ -1,27 +1,13 @@
 package fr.inra.oresing.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.inra.oresing.model.OreSiUser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.security.Key;
-import java.util.Date;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 @Slf4j
 @Component
@@ -46,7 +32,7 @@ public class AuthHandler implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
         log.debug("postHandle for " + OreSiContext.get());
-       authHelper.cleanContext();
+        authHelper.cleanContext();
     }
 
 }
