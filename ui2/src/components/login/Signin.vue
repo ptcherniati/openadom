7a9c5f4dd02a56fbec894b1047c7cf18f9eea17a
Aurore Lecointe <lecointe@codelutin.com>
2021-04-27 16:34:10 +0200

    Vee validate: vérification des inputs de login

--- ui2/src/components/login/Signin.vue
+++ ui2/src/components/login/Signin.vue
@@ -1,23 +1,66 @@
 <template>
   <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
     <section>
-      <InputWithValidation
+      <ValidationProvider
         rules="required|email"
-        type="email"
-        :placeholder="$t('login.email-placeholder')"
-        :label="$t('login.email')"
-        v-model="email"
-      />
+        name="email"
+        v-slot="{ errors, valid }"
+        v-id="email"
+      >
+        <b-field
+          class="input-field"
+          :type="{
+            'is-normal': disabled,
+            'is-danger': errors && errors.length > 0,
+            'is-success': valid,
+          }"
+          :message="errors[0]"
+        >
+          <template slot="label">
+            {{ $t("login.email") }}
+            <span class="mandatory">
+              {{ $t("validation.obligatoire") }}
+            </span>
+          </template>
+          <b-input
+            type="email"
+            v-model="email"
+            :placeholder="$t('login.email-placeholder')"
+          >
+          </b-input>
+        </b-field>
+      </ValidationProvider>
 
-      <InputWithValidation
+      <ValidationProvider
         rules="required"
-        type="password"
-        :passwordReveal="true"
-        :placeholder="$t('login.pwd-placeholder')"
-        :label="$t('login.pwd')"
+        name="email"
+        v-slot="{ errors, valid }"
         vid="password"
-        v-model="password"
-      />
+      >
+        <b-field
+          class="input-field"
+          :type="{
+            'is-normal': disabled,
+            'is-danger': errors && errors.length > 0,
+            'is-success': valid,
+          }"
+          :message="errors[0]"
+        >
+          <template slot="label">
+            {{ $t("login.pwd") }}
+            <span class="mandatory">
+              {{ $t("validation.obligatoire") }}
+            </span>
+          </template>
+          <b-input
+            type="password"
+            v-model="password"
+            :placeholder="$t('login.pwd-placeholder')"
+            :password-reveal="true"
+          >
+          </b-input>
+        </b-field>
+      </ValidationProvider>
     </section>
 
     <div class="buttons">
@@ -33,11 +76,10 @@
 
 <script>
 import { Component, Vue } from "vue-property-decorator";
-import { ValidationObserver } from "vee-validate";
-import InputWithValidation from "../validation/InputWithValidation.vue";
+import { ValidationObserver, ValidationProvider } from "vee-validate";
 
 @Component({
-  components: { ValidationObserver, InputWithValidation },
+  components: { ValidationObserver, ValidationProvider },
 })
 export default class SignIn extends Vue {
   email = "";
