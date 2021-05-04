d18ac397729dbefff8b17e479c3528a01d3218c0
Aurore Lecointe <lecointe@codelutin.com>
2021-05-04 20:40:54 +0200

    Donne la locale dans le header des requêtes.

--- ui2/src/services/UserPreferencesService.js
+++ ui2/src/services/UserPreferencesService.js
@@ -1,24 +1,11 @@
 import app from "@/main";
-import { Locales } from "@/utils/LocaleUtils";
-import { Fetcher } from "./Fetcher";
-
-const LOCAL_STORAGE_LANG = "lang";
+import { Fetcher, LOCAL_STORAGE_LANG } from "./Fetcher";
 
 export class UserPreferencesService extends Fetcher {
   static INSTANCE = new UserPreferencesService();
 
   constructor() {
     super();
-  }
-
-  getUserPrefLocale() {
-    const browserLocale = window.navigator.language.substring(0, 2);
-
-    return (
-      localStorage.getItem(LOCAL_STORAGE_LANG) ||
-      (Object.values(Locales).includes(browserLocale) && browserLocale) ||
-      Locales.FRENCH
-    );
   }
 
   setUserPrefLocale(locale) {
