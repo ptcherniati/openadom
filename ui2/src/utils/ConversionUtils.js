552603c4d8d894dcaa6c5ee1f81803e58b4d16fe
Aurore Lecointe <lecointe@codelutin.com>
2021-06-11 10:23:42 +0200

    Enlève l'effet de bord modifiant les ref initiales

--- ui2/src/utils/ConversionUtils.js
+++ ui2/src/utils/ConversionUtils.js
@@ -1,32 +1,24 @@
-export function convertReferencesToTrees(references) {
-  const array = references.filter((ref) => {
+export function convertReferencesToTrees(initialReference) {
+  const references = JSON.parse(JSON.stringify(initialReference));
+  const parents = references.filter((ref) => {
     return !references.some(
       (r) => r.children && r.children.length !== 0 && r.children.some((c) => c === ref.id)
     );
   });
-  return convert(array, references);
+  return replaceChildrenIdByObject(parents, references);
 }
 
-function convert(references, initialRef) {
+function replaceChildrenIdByObject(references, initialRef) {
   references.forEach((ref) => {
     if (ref.children && ref.children.length !== 0) {
-      const children = ref.children.map((c) => initialRef.find((r) => r.id === c));
-      ref.children = convert(children, initialRef);
+      const children = ref.children.map((c) => {
+        const index = initialRef.findIndex((r) => r.id === c);
+        const [child] = initialRef.splice(index, 1);
+        return child;
+      });
+      ref.children = replaceChildrenIdByObject(children, initialRef);
     } else {
       return ref;
-      //   const parentIndex = initialRef.findIndex(
-      //     (r) => r.children && r.children.length !== 0 && r.children.some((c) => c === ref.id)
-      //   );
-      //   if (!parentIndex) {
-      //     return ref;
-      //   } else {
-      //     return (initialRef[parentIndex].children = initialRef[parentIndex].children.map((c) => {
-      //       if (c === ref.id) {
-      //         return ref;
-      //       }
-      //       return c;
-      //     }));
-      //   }
     }
   });
   return references;
