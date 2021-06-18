de138eb3f7841dab4a53b92cfb6f5f4269d0bac1
Aurore Lecointe <lecointe@codelutin.com>
2021-06-18 11:00:09 +0200

    Premier jet d'un tableau avec deux en-tête

--- ui2/src/views/dataset/DataSetTableView.vue
+++ ui2/src/views/dataset/DataSetTableView.vue
@@ -1,6 +1,31 @@
 <template>
   <PageView>
     <h1 class="title main-title">{{ dataSetId }}</h1>
+
+    <div class="b-table">
+      <div
+        class="DataSetTableView-wrapper table-wrapper has-mobile-cards has-sticky-header"
+        style="height: 100%"
+      >
+        <table class="table is-striped">
+          <thead>
+            <tr>
+              <th colspan="2">Variable 1</th>
+            </tr>
+            <tr>
+              <th colspan="1">Composant 1</th>
+              <th colspan="1">Composant 2</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Donnée 1.a</td>
+              <td>Donnée 1.b</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </PageView>
 </template>
 
@@ -16,3 +41,9 @@ export default class DataSetTableView extends Vue {
   @Prop() dataSetId;
 }
 </script>
+
+<style lang="scss" scoped>
+.DataSetTableView-wrapper {
+  margin-bottom: 1.5rem;
+}
+</style>
