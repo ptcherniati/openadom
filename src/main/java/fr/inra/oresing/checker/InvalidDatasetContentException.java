bb1505820c552743802ddcb903c307253269bf33
Brendan Le Ny <bleny@codelutin.com>
2021-06-22 11:21:21 +0200

    Factorise du code

--- src/main/java/fr/inra/oresing/checker/InvalidDatasetContentException.java
+++ src/main/java/fr/inra/oresing/checker/InvalidDatasetContentException.java
@@ -6,12 +6,11 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.Sets;
 import fr.inra.oresing.OreSiTechnicalException;
-import fr.inra.oresing.ValidationLevel;
 import fr.inra.oresing.rest.CsvRowValidationCheckResult;
+import fr.inra.oresing.rest.DefaultValidationCheckResult;
 import fr.inra.oresing.rest.ValidationCheckResult;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Exception levée si un jeu de données est incorrect (aussi bien entêtes que contenu)
@@ -26,119 +25,48 @@ public class InvalidDatasetContentException extends OreSiTechnicalException {
     }
 
     public static InvalidDatasetContentException forUnexpectedHeaderColumn(String expected, String actual, int headerLine) {
-        return new InvalidDatasetContentException(List.of(new CsvRowValidationCheckResult(new ValidationCheckResult() {
-            @Override
-            public ValidationLevel getLevel() {
-                return ValidationLevel.ERROR;
-            }
-
-            @Override
-            public String getMessage() {
-                return "unexpectedHeaderColumn";
-            }
-
-            @Override
-            public Map<String, Object> getMessageParams() {
-                return ImmutableMap.of(
-                        "actualHeaderColumn", actual,
-                        "expectedHeaderColumn", expected
-                );
-            }
-        }, headerLine)));
+        return newInvalidDatasetContentException(headerLine, "unexpectedHeaderColumn", ImmutableMap.of(
+                "actualHeaderColumn", actual,
+                "expectedHeaderColumn", expected
+        ));
     }
 
     public static InvalidDatasetContentException forHeaderColumnPatternNotMatching(String expectedPattern, String actual, int headerLine) {
-        return new InvalidDatasetContentException(List.of(new CsvRowValidationCheckResult(new ValidationCheckResult() {
-            @Override
-            public ValidationLevel getLevel() {
-                return ValidationLevel.ERROR;
-            }
-
-            @Override
-            public String getMessage() {
-                return "headerColumnPatternNotMatching";
-            }
-
-            @Override
-            public Map<String, Object> getMessageParams() {
-                return ImmutableMap.of(
-                        "actualHeaderColumn", actual,
-                        "expectedHeaderColumnPattern", expectedPattern
-                );
-            }
-        }, headerLine)));
+        return newInvalidDatasetContentException(headerLine, "headerColumnPatternNotMatching", ImmutableMap.of(
+                "actualHeaderColumn", actual,
+                "expectedHeaderColumnPattern", expectedPattern
+        ));
     }
 
     public static InvalidDatasetContentException forUnexpectedTokenCount(int expectedTokenCount, String actualHeader, int actualTokenCount, int headerLine) {
-        return new InvalidDatasetContentException(List.of(new CsvRowValidationCheckResult(new ValidationCheckResult() {
-            @Override
-            public ValidationLevel getLevel() {
-                return ValidationLevel.ERROR;
-            }
-
-            @Override
-            public String getMessage() {
-                // "On doit pouvoir repérer " + tokens.size() + " informations dans l'entête " + actualHeader + ", or seulement " + matcher.groupCount() + " détectés"
-                return "unexpectedTokenCount";
-            }
-
-            @Override
-            public Map<String, Object> getMessageParams() {
-                return ImmutableMap.of(
-                        "expectedTokenCount", expectedTokenCount,
-                        "actualHeader", actualHeader,
-                        "actualTokenCount", actualTokenCount
-                );
-            }
-        }, headerLine)));
+        return newInvalidDatasetContentException(headerLine, "unexpectedTokenCount", ImmutableMap.of(
+                "expectedTokenCount", expectedTokenCount,
+                "actualHeader", actualHeader,
+                "actualTokenCount", actualTokenCount
+        ));
     }
 
-    public static InvalidDatasetContentException forInvalidHeaders(ImmutableSet<String> expectedColumns, ImmutableMultiset<String> actualColumns, int headerLine) {
-        return new InvalidDatasetContentException(List.of(new CsvRowValidationCheckResult(new ValidationCheckResult() {
-            @Override
-            public ValidationLevel getLevel() {
-                return ValidationLevel.ERROR;
-            }
-
-            @Override
-            public String getMessage() {
-                return "invalidHeaders";
-            }
-
-            @Override
-            public Map<String, Object> getMessageParams() {
-                ImmutableSet<String> actualColumnsAsSet = ImmutableSet.copyOf(actualColumns);
-                ImmutableSet<String> missingColumns = Sets.difference(expectedColumns, actualColumnsAsSet).immutableCopy();
-                ImmutableSet<String> unknownColumns = Sets.difference(actualColumnsAsSet, expectedColumns).immutableCopy();
-                return ImmutableMap.of(
-                        "expectedColumns", expectedColumns,
-                        "actualColumns", actualColumns.elementSet(),
-                        "missingColumns", missingColumns,
-                        "unknownColumns", unknownColumns
-                );
-            }
-        }, headerLine)));
+    public static InvalidDatasetContentException forInvalidHeaders(ImmutableSet<String> expectedColumns, ImmutableSet<String> actualColumns, int headerLine) {
+        ImmutableSet<String> missingColumns = Sets.difference(expectedColumns, actualColumns).immutableCopy();
+        ImmutableSet<String> unknownColumns = Sets.difference(actualColumns, expectedColumns).immutableCopy();
+        return newInvalidDatasetContentException(headerLine, "invalidHeaders", ImmutableMap.of(
+                "expectedColumns", expectedColumns,
+                "actualColumns", actualColumns,
+                "missingColumns", missingColumns,
+                "unknownColumns", unknownColumns
+        ));
     }
 
     public static InvalidDatasetContentException forDuplicatedHeaders(int headerLine, ImmutableSet<String> duplicatedHeaders) {
-        return new InvalidDatasetContentException(List.of(new CsvRowValidationCheckResult(new ValidationCheckResult() {
-            @Override
-            public ValidationLevel getLevel() {
-                return ValidationLevel.ERROR;
-            }
+        return newInvalidDatasetContentException(headerLine, "duplicatedHeaders", ImmutableMap.of(
+                "duplicatedHeaders", duplicatedHeaders
+        ));
+    }
 
-            @Override
-            public String getMessage() {
-                return "duplicatedHeaders";
-            }
-
-            @Override
-            public Map<String, Object> getMessageParams() {
-                return ImmutableMap.of(
-                        "duplicatedHeaders", duplicatedHeaders
-                );
-            }
-        }, headerLine)));
+    private static InvalidDatasetContentException newInvalidDatasetContentException(int headerLine, String message, ImmutableMap<String, Object> messageParams) {
+        ValidationCheckResult validationCheckResult = DefaultValidationCheckResult.error(message, messageParams);
+        CsvRowValidationCheckResult csvRowValidationCheckResult = new CsvRowValidationCheckResult(validationCheckResult, headerLine);
+        return new InvalidDatasetContentException(List.of(csvRowValidationCheckResult));
     }
 
     public static void checkHeader(ImmutableSet<String> expectedColumns, ImmutableMultiset<String> actualColumns, int headerLine) {
@@ -149,8 +77,9 @@ public class InvalidDatasetContentException extends OreSiTechnicalException {
         if (!duplicatedHeaders.isEmpty()) {
             throw InvalidDatasetContentException.forDuplicatedHeaders(headerLine, duplicatedHeaders);
         }
-        if (!expectedColumns.equals(actualColumns.elementSet())) {
-            throw InvalidDatasetContentException.forInvalidHeaders(expectedColumns, actualColumns, headerLine);
+        ImmutableSet<String> actualColumnsAsSet = actualColumns.elementSet();
+        if (!expectedColumns.equals(actualColumnsAsSet)) {
+            throw InvalidDatasetContentException.forInvalidHeaders(expectedColumns, actualColumnsAsSet, headerLine);
         }
     }
 
