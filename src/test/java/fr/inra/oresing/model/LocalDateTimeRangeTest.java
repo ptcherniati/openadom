f66420476f1fe30305f0e77cf473b53ce51e2f7a
Brendan Le Ny <bleny@codelutin.com>
2021-06-25 16:53:13 +0200

    fixup! L'année inclue 31 décembre

--- src/test/java/fr/inra/oresing/model/LocalDateTimeRangeTest.java
+++ src/test/java/fr/inra/oresing/model/LocalDateTimeRangeTest.java
@@ -12,7 +12,7 @@ public class LocalDateTimeRangeTest {
         {
             LocalDateTimeRange localDateTimeRange = LocalDateTimeRange.forYear(Year.of(2020));
             String sql = localDateTimeRange.toSqlExpression();
-            Assert.assertEquals("[\"2020-01-01 00:00:00\",\"2020-12-31 00:00:00\")", sql);
+            Assert.assertEquals("[\"2020-01-01 00:00:00\",\"2021-01-01 00:00:00\")", sql);
             LocalDateTimeRange parsed = LocalDateTimeRange.parseSql(sql);
             Assert.assertEquals(localDateTimeRange, parsed);
         }
