e1dfadeefcdf3369408fb972131e2140c6555c56
Brendan Le Ny <bleny@codelutin.com>
2021-06-30 15:23:21 +0200

    Renforce le contrôle des dates en fonction du format

---
+++ src/test/java/fr/inra/oresing/checker/DateLineCheckerTest.java
@@ -0,0 +1,17 @@
+package fr.inra.oresing.checker;
+
+import fr.inra.oresing.model.VariableComponentKey;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class DateLineCheckerTest {
+
+    @Test
+    public void testCheck() {
+        DateLineChecker dateLineChecker = new DateLineChecker(new VariableComponentKey("ignored", "ignored"), "dd/MM/yyyy");
+        Assert.assertTrue(dateLineChecker.check("12/01/2021").isSuccess());
+        Assert.assertFalse(dateLineChecker.check("06/21").isSuccess());
+        Assert.assertFalse(dateLineChecker.check("04/03/10").isSuccess());
+    }
+}
