f421029dcdc84bd0cd8804037d907dad276cccee
Lucile Varloteaux <lucile.varloteaux@inrae.fr>
2021-07-29 11:30:15 +0200

    ajout icon agrandir card et modal card pour description

--- ui2/src/main.js
+++ ui2/src/main.js
@@ -28,6 +28,7 @@ import {
   faVial,
   faCaretRight,
   faArrowLeft,
+  faExternalLinkSquareAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 library.add(
@@ -53,7 +54,8 @@ library.add(
   faDownload,
   faVial,
   faCaretRight,
-  faArrowLeft
+  faArrowLeft,
+  faExternalLinkSquareAlt
 );
 Vue.component("vue-fontawesome", FontAwesomeIcon);
 
--- ui2/src/views/application/ApplicationsView.vue
+++ ui2/src/views/application/ApplicationsView.vue
@@ -14,21 +14,49 @@
             <div class="card">
               <div class="card-header">
                 <div class="title card-header-title" style="margin-top: 0; text-transform: uppercase; margin-bottom: 0px;">
-                  <h2 field="name"> {{ application.name }}</h2>
+                  <p field="name"> {{ application.name }}</p>
                 </div>
+                <section>
+                  <b-button icon-left="external-link-square-alt"
+                      type="is-primary"
+                      size="is-medium"
+                      @click="isCardModalActive = true" style="margin: 5px; opacity: 50%; color: #00a3a6; background-color: transparent;"/>
+                  <b-modal v-model="isCardModalActive">
+                    <div class="card">
+                      <div class="card-header">
+                        <div class="title card-header-title" style="margin-top: 0; text-transform: uppercase; margin-bottom: 0px;">
+                          <p field="name"> {{ application.name }}</p>
+                        </div>
+                      </div>
+                      <div class="card-content">
+                        <div class="content">
+                          <h4>Bienvenue sur le SI du Système d’Information sur les Produits Résiduaires Organiques (SI PRO) du SOERE PRO (Système d’observation et d’expérimentation pour la recherche en environnement).</h4>
+                          <p>Le SOERE PRO est un observatoire de recherche en environnement composé de dispositifs expérimentaux au champ dédiés à l’étude des effets à long terme du recyclage agricole des PRO. Le réseau de sites est labellisé en tant que SOERE par ALLENVI depuis 2011, avec renouvellement de la labellisation en 2015. Il est aussi intégré à l’infrastructure ANAEE-France depuis 2013 ANNAEE France
+                            Le Système d’information sur les Produits Résiduaires Organiques (SI PRO) est élaboré par l’INRA UMR ECOSYS et ECOINFORMATIQUE, en partenariat avec les équipes pilotes des sites du SOERE PRO (CIRAD, INRA) et des partenaires filières (Arvalis, LDAR, IFV, ITAB).Le SI archive les données acquises sur des dispositifs expérimentaux au champ du SOERE PRO et de partenaires filières, il archive également les métadonnées associées à ces jeux de données (ex. descriptif du dispositif, contexte agro-pédoclimatique). Il a aussi pour vocation d’archiver les caractéristiques analytiques de PRO pouvant être épandus en agriculture ainsi que leurs variables d’obtentions (ex. composition, descriptif du procédé de traitement, nomenclature…).</p>
+                          <h4>Comment accéder aux données ?</h4>
+                          <p>Voir quelles sont les données actuellement disponibles dans la base de données.
+                            Le système d'information contient des données qui sont en libre accès et des données accessibles après validation d'une demande spécifique auprès des responsables scientifiques. Dans tous les cas, vous devez vous connecter avant de pouvoir interroger la base de données et extraire des données pour vos besoins.</p>
+                        </div>
+                      </div>
+                      <div class="card-footer">
+                        <div class="card-footer-item">
+                          <a icon-left="drafting-compass"
+                              @click="displayReferencesManagement(application)">
+                            {{ $t("applications.references") }}</a>
+                        </div>
+                        <div class="card-footer-item">
+                          <a icon-left="poll"
+                              @click="displayDataSetManagement(application)">
+                            {{ $t("applications.dataset") }}</a>
+                        </div>
+                      </div>
+                    </div>
+                  </b-modal>
+                </section>
               </div>
               <div class="card-content">
                 <div class="content">
-                  <p field="creationDate">{{ new Date(application.creationDate) }}</p><section>
-                  <b-collapse :open="false">
-                    <button class="button is-primary" slot="trigger">Description</button>
-                    <div class="notification">
-                      <div class="content">
-                        <p >description de l'ORE mis dans le fichier yaml</p>
-                      </div>
-                    </div>
-                  </b-collapse>
-                </section>
+                  <p field="creationDate">{{ new Date(application.creationDate) }}</p>
                 </div>
               </div>
               <div class="card-footer">
@@ -97,6 +125,7 @@ export default class ApplicationsView extends Vue {
 
   applications = [];
   canCreateApplication = LoginService.INSTANCE.getAuthenticatedUser().authorizedForApplicationCreation;
+  isCardModalActive=false;
 
   created() {
     this.init();
