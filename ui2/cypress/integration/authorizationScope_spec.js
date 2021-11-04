5d41217d357cdeaeefc877fa8b51ea8af4e239a3
TCHERNIATINSKY <philippe.tcherniatinsky@inrae.fr>
2021-11-04 11:57:35 +0100

    Correction du test cypress ajout des références

--- ui2/cypress/integration/authorizationScope_spec.js
+++ ui2/cypress/integration/authorizationScope_spec.js
@@ -24,6 +24,20 @@ describe('test authorization application', () => {
             'http://localhost:8081/api/v1/applications/monsore/dataType/pem/authorization',
             response).as('getShowAuthorizations')
 
+        const responseSites= response => cy.intercept(
+            'GET',
+            'http://localhost:8081/api/v1/applications/monsore/references/sites',
+            response).as('getSites')
+
+        const responseProjet= response => cy.intercept(
+            'GET',
+            'http://localhost:8081/api/v1/applications/monsore/references/projet',
+            response).as('getProjet')
+        const responseTypeSites= response => cy.intercept(
+            'GET',
+            'http://localhost:8081/api/v1/applications/monsore/references/type_de_sites',
+            response).as('getTypeSites')
+
         cy.fixture('applications/ore/monsore/dataType_response.json').then(resolveDataTypes)
         cy.fixture('authorisation/monsore/show_authorization_table.json').then(responseAuthorization)
 
@@ -31,6 +45,9 @@ describe('test authorization application', () => {
         cy.wait('@getShowAuthorizations')
 
         cy.fixture('authorisation/monsore/new_authorization_request.json').then(resolveAuthorization)
+        cy.fixture('references/monsore/sites.json').then(responseSites)
+        cy.fixture('references/monsore/projet.json').then(responseProjet)
+        cy.fixture('references/monsore/type_de_sites.json').then(responseTypeSites)
 
         cy.visit(Cypress.env('monsore_new_authorization_url'))
         cy.wait(['@getAuthorization','@getDataTypes'])
