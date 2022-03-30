147246f3342c7d01d9b1c6e546a38a15b23fbaaf
Brendan Le Ny <bleny@codelutin.com>
2022-03-30 11:14:23 +0200

    Clarifie le code dans ReferenceService

--- src/main/java/fr/inra/oresing/rest/ReferenceService.java
+++ src/main/java/fr/inra/oresing/rest/ReferenceService.java
@@ -18,6 +18,7 @@ import fr.inra.oresing.model.Application;
 import fr.inra.oresing.model.ColumnPresenceConstraint;
 import fr.inra.oresing.model.ComputedValueUsage;
 import fr.inra.oresing.model.Configuration;
+import fr.inra.oresing.model.GroovyDataInjectionConfiguration;
 import fr.inra.oresing.model.ReferenceColumn;
 import fr.inra.oresing.model.ReferenceColumnMultipleValue;
 import fr.inra.oresing.model.ReferenceColumnSingleValue;
@@ -97,7 +98,11 @@ public class ReferenceService {
                     ReferenceColumn referenceColumn = new ReferenceColumn(entry.getKey());
                     Multiplicity multiplicity = multiplicityPerColumns.getOrDefault(referenceColumn, Multiplicity.ONE);
                     Configuration.ReferenceStaticNotComputedColumnDescription referenceStaticNotComputedColumnDescription = MoreObjects.firstNonNull(entry.getValue(), new Configuration.ReferenceStaticNotComputedColumnDescription());
-                    return staticColumnDescriptionToColumn(referenceValueRepository, referenceColumn, multiplicity, referenceStaticNotComputedColumnDescription);
+                    ColumnPresenceConstraint presenceConstraint = referenceStaticNotComputedColumnDescription.getPresenceConstraint();
+                    ReferenceImporterContext.Column column = Optional.ofNullable(referenceStaticNotComputedColumnDescription.getDefaultValue())
+                            .map(defaultValueConfiguration -> staticColumnDescriptionToColumn(referenceColumn, presenceConstraint, multiplicity, referenceValueRepository, defaultValueConfiguration))
+                            .orElseGet(() -> staticColumnDescriptionToColumn(referenceColumn, presenceConstraint, multiplicity));
+                    return column;
                 }).collect(ImmutableSet.toImmutableSet());
 
         ImmutableSet<ReferenceImporterContext.Column> computedColumns = referenceDescription.getComputedColumns().entrySet().stream()
@@ -105,7 +110,7 @@ public class ReferenceService {
                     ReferenceColumn referenceColumn = new ReferenceColumn(entry.getKey());
                     Configuration.ReferenceStaticComputedColumnDescription referenceStaticComputedColumnDescription = entry.getValue();
                     Multiplicity multiplicity = multiplicityPerColumns.getOrDefault(referenceColumn, Multiplicity.ONE);
-                    return computedColumnDescriptionToColumn(referenceValueRepository, referenceColumn, referenceStaticComputedColumnDescription, multiplicity);
+                    return computedColumnDescriptionToColumn(referenceValueRepository, referenceColumn, multiplicity, referenceStaticComputedColumnDescription);
                 }).collect(ImmutableSet.toImmutableSet());
 
         ImmutableSet<ReferenceImporterContext.Column> dynamicColumns = referenceDescription.getDynamicColumns().entrySet().stream()
@@ -189,36 +194,10 @@ public class ReferenceService {
         return valuedDynamicColumns;
     }
 
-    private ReferenceImporterContext.Column staticColumnDescriptionToColumn(ReferenceValueRepository referenceValueRepository, ReferenceColumn referenceColumn, Multiplicity multiplicity, Configuration.ReferenceStaticNotComputedColumnDescription referenceStaticNotComputedColumnDescription) {
-        ColumnPresenceConstraint presenceConstraint = referenceStaticNotComputedColumnDescription.getPresenceConstraint();
+    private ReferenceImporterContext.Column staticColumnDescriptionToColumn(ReferenceColumn referenceColumn, ColumnPresenceConstraint presenceConstraint, Multiplicity multiplicity) {
         ReferenceImporterContext.Column column;
         if (multiplicity == Multiplicity.ONE) {
-            column = Optional.ofNullable(referenceStaticNotComputedColumnDescription.getDefaultValue()).map(defaultValueConfiguration -> {
-                Expression<String> computationExpression = StringGroovyExpression.forExpression(defaultValueConfiguration.getExpression());
-                Set<String> configurationReferences = defaultValueConfiguration.getReferences();
-                ImmutableMap<String, Object> contextForExpression = groovyContextHelper.getGroovyContextForReferences(referenceValueRepository, configurationReferences);
-                Preconditions.checkState(defaultValueConfiguration.getDatatypes().isEmpty(), "à ce stade, on ne gère pas la chargement de données");
-                ReferenceImporterContext.Column oneValueStaticColumn = new ReferenceImporterContext.OneValueStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.USE_COMPUTED_AS_DEFAULT_VALUE) {
-                    @Override
-                    String getExpectedHeader() {
-                        return referenceColumn.getColumn();
-                    }
-
-                    @Override
-                    Optional<ReferenceColumnValue> computeValue(ReferenceDatum referenceDatum) {
-                        ImmutableMap<String, Object> evaluationContext = ImmutableMap.<String, Object>builder()
-                                .putAll(contextForExpression)
-                                .putAll(referenceDatum.getEvaluationContext())
-                                .build();
-                        String evaluate = computationExpression.evaluate(evaluationContext);
-                        Optional<ReferenceColumnValue> computedValue = Optional.ofNullable(evaluate)
-                                .filter(StringUtils::isNotEmpty)
-                                .map(ReferenceColumnSingleValue::new);
-                        return computedValue;
-                    }
-                };
-                return oneValueStaticColumn;
-            }).orElseGet(() -> new ReferenceImporterContext.OneValueStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.NOT_COMPUTED) {
+            column = new ReferenceImporterContext.OneValueStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.NOT_COMPUTED) {
                 @Override
                 String getExpectedHeader() {
                     return referenceColumn.getColumn();
@@ -228,33 +207,9 @@ public class ReferenceService {
                 Optional<ReferenceColumnValue> computeValue(ReferenceDatum referenceDatum) {
                     throw new UnsupportedOperationException("pas de valeur par défaut pour " + referenceColumn);
                 }
-            });
+            };
         } else if (multiplicity == Multiplicity.MANY) {
-            column = Optional.ofNullable(referenceStaticNotComputedColumnDescription.getDefaultValue()).map(defaultValueConfiguration -> {
-                Expression<Set<String>> computationExpression = StringSetGroovyExpression.forExpression(defaultValueConfiguration.getExpression());
-                Set<String> configurationReferences = defaultValueConfiguration.getReferences();
-                ImmutableMap<String, Object> contextForExpression = groovyContextHelper.getGroovyContextForReferences(referenceValueRepository, configurationReferences);
-                Preconditions.checkState(defaultValueConfiguration.getDatatypes().isEmpty(), "à ce stade, on ne gère pas la chargement de données");
-                ReferenceImporterContext.Column manyValuesStaticColumn = new ReferenceImporterContext.ManyValuesStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.USE_COMPUTED_AS_DEFAULT_VALUE) {
-                    @Override
-                    String getExpectedHeader() {
-                        return referenceColumn.getColumn();
-                    }
-
-                    @Override
-                    Optional<ReferenceColumnValue> computeValue(ReferenceDatum referenceDatum) {
-                        ImmutableMap<String, Object> evaluationContext = ImmutableMap.<String, Object>builder()
-                                .putAll(contextForExpression)
-                                .putAll(referenceDatum.getEvaluationContext())
-                                .build();
-                        Set<String> evaluate = computationExpression.evaluate(evaluationContext);
-                        Optional<ReferenceColumnValue> computedValue = Optional.ofNullable(evaluate)
-                                .map(ReferenceColumnMultipleValue::new);
-                        return computedValue;
-                    }
-                };
-                return manyValuesStaticColumn;
-            }).orElseGet(() -> new ReferenceImporterContext.ManyValuesStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.NOT_COMPUTED) {
+            column = new ReferenceImporterContext.ManyValuesStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.NOT_COMPUTED) {
                 @Override
                 String getExpectedHeader() {
                     return referenceColumn.getColumn();
@@ -264,26 +219,22 @@ public class ReferenceService {
                 Optional<ReferenceColumnValue> computeValue(ReferenceDatum referenceDatum) {
                     throw new UnsupportedOperationException("pas de valeur par défaut pour " + referenceColumn);
                 }
-            });
+            };
         } else {
             throw new IllegalStateException("multiplicity = " + multiplicity);
         }
         return column;
     }
 
-    private ReferenceImporterContext.Column computedColumnDescriptionToColumn(ReferenceValueRepository referenceValueRepository, ReferenceColumn referenceColumn, Configuration.ReferenceStaticComputedColumnDescription referenceStaticComputedColumnDescription, Multiplicity multiplicity) {
-        Configuration.GroovyConfiguration computation = referenceStaticComputedColumnDescription.getComputation();
-        ColumnPresenceConstraint presenceConstraint = ColumnPresenceConstraint.ABSENT;
+    private ReferenceImporterContext.Column staticColumnDescriptionToColumn(ReferenceColumn referenceColumn, ColumnPresenceConstraint presenceConstraint, Multiplicity multiplicity, ReferenceValueRepository referenceValueRepository, Configuration.GroovyConfiguration defaultValueConfiguration) {
+        ImmutableMap<String, Object> contextForExpression = computeGroovyContext(referenceValueRepository, defaultValueConfiguration);
         ReferenceImporterContext.Column column;
-        Set<String> configurationReferences = computation.getReferences();
-        ImmutableMap<String, Object> contextForExpression = groovyContextHelper.getGroovyContextForReferences(referenceValueRepository, configurationReferences);
-        Preconditions.checkState(computation.getDatatypes().isEmpty(), "à ce stade, on ne gère pas la chargement de données");
         if (multiplicity == Multiplicity.ONE) {
-            Expression<String> computationExpression = StringGroovyExpression.forExpression(computation.getExpression());
-            column = new ReferenceImporterContext.OneValueStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.USE_COMPUTED_VALUE) {
+            Expression<String> computationExpression = StringGroovyExpression.forExpression(defaultValueConfiguration.getExpression());
+            column = new ReferenceImporterContext.OneValueStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.USE_COMPUTED_AS_DEFAULT_VALUE) {
                 @Override
                 String getExpectedHeader() {
-                    throw new UnsupportedOperationException("la colonne " + referenceColumn + " est calculée, il n'y a pas d'entête spécifié");
+                    return referenceColumn.getColumn();
                 }
 
                 @Override
@@ -300,11 +251,11 @@ public class ReferenceService {
                 }
             };
         } else if (multiplicity == Multiplicity.MANY) {
-            Expression<Set<String>> computationExpression = StringSetGroovyExpression.forExpression(computation.getExpression());
-            column = new ReferenceImporterContext.ManyValuesStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.USE_COMPUTED_VALUE) {
+            Expression<Set<String>> computationExpression = StringSetGroovyExpression.forExpression(defaultValueConfiguration.getExpression());
+            column = new ReferenceImporterContext.ManyValuesStaticColumn(referenceColumn, presenceConstraint, ComputedValueUsage.USE_COMPUTED_AS_DEFAULT_VALUE) {
                 @Override
                 String getExpectedHeader() {
-                    throw new UnsupportedOperationException("la colonne " + referenceColumn + " est calculée, il n'y a pas d'entête spécifié car elle ne doit pas être dans le CSV");
+                    return referenceColumn.getColumn();
                 }
 
                 @Override
@@ -323,6 +274,74 @@ public class ReferenceService {
             throw new IllegalStateException("multiplicity = " + multiplicity);
         }
         return column;
+    }
+
+    private ReferenceImporterContext.Column computedColumnDescriptionToColumn(ReferenceValueRepository referenceValueRepository, ReferenceColumn referenceColumn, Multiplicity multiplicity, Configuration.ReferenceStaticComputedColumnDescription referenceStaticComputedColumnDescription) {
+        ReferenceImporterContext.Column column;
+        if (multiplicity == Multiplicity.ONE) {
+            column = newComputedColumn(referenceColumn, referenceStaticComputedColumnDescription, referenceValueRepository);
+        } else if (multiplicity == Multiplicity.MANY) {
+            column = newComputedManyColumn(referenceColumn, referenceStaticComputedColumnDescription, referenceValueRepository);
+        } else {
+            throw new IllegalStateException("multiplicity = " + multiplicity);
+        }
+        return column;
+    }
+
+    private ReferenceImporterContext.Column newComputedManyColumn(ReferenceColumn referenceColumn, Configuration.ReferenceStaticComputedColumnDescription referenceStaticComputedColumnDescription, ReferenceValueRepository referenceValueRepository) {
+        Configuration.GroovyConfiguration computation = referenceStaticComputedColumnDescription.getComputation();
+        ImmutableMap<String, Object> contextForExpression = computeGroovyContext(referenceValueRepository, computation);
+        Expression<Set<String>> computationExpression = StringSetGroovyExpression.forExpression(computation.getExpression());
+        return new ReferenceImporterContext.ManyValuesStaticColumn(referenceColumn, ColumnPresenceConstraint.ABSENT, ComputedValueUsage.USE_COMPUTED_VALUE) {
+            @Override
+            String getExpectedHeader() {
+                throw new UnsupportedOperationException("la colonne " + referenceColumn + " est calculée, il n'y a pas d'entête spécifié car elle ne doit pas être dans le CSV");
+            }
+
+            @Override
+            Optional<ReferenceColumnValue> computeValue(ReferenceDatum referenceDatum) {
+                ImmutableMap<String, Object> evaluationContext = ImmutableMap.<String, Object>builder()
+                        .putAll(contextForExpression)
+                        .putAll(referenceDatum.getEvaluationContext())
+                        .build();
+                Set<String> evaluate = computationExpression.evaluate(evaluationContext);
+                Optional<ReferenceColumnValue> computedValue = Optional.ofNullable(evaluate)
+                        .map(ReferenceColumnMultipleValue::new);
+                return computedValue;
+            }
+        };
+    }
+
+    private ReferenceImporterContext.Column newComputedColumn(ReferenceColumn referenceColumn, Configuration.ReferenceStaticComputedColumnDescription referenceStaticComputedColumnDescription, ReferenceValueRepository referenceValueRepository) {
+        Configuration.GroovyConfiguration computation = referenceStaticComputedColumnDescription.getComputation();
+        ImmutableMap<String, Object> contextForExpression = computeGroovyContext(referenceValueRepository, computation);
+        Expression<String> computationExpression = StringGroovyExpression.forExpression(computation.getExpression());
+        return new ReferenceImporterContext.OneValueStaticColumn(referenceColumn, ColumnPresenceConstraint.ABSENT, ComputedValueUsage.USE_COMPUTED_VALUE) {
+            @Override
+            String getExpectedHeader() {
+                throw new UnsupportedOperationException("la colonne " + referenceColumn + " est calculée, il n'y a pas d'entête spécifié");
+            }
+
+            @Override
+            Optional<ReferenceColumnValue> computeValue(ReferenceDatum referenceDatum) {
+                ImmutableMap<String, Object> evaluationContext = ImmutableMap.<String, Object>builder()
+                        .putAll(contextForExpression)
+                        .putAll(referenceDatum.getEvaluationContext())
+                        .build();
+                String evaluate = computationExpression.evaluate(evaluationContext);
+                Optional<ReferenceColumnValue> computedValue = Optional.ofNullable(evaluate)
+                        .filter(StringUtils::isNotEmpty)
+                        .map(ReferenceColumnSingleValue::new);
+                return computedValue;
+            }
+        };
+    }
+
+    private ImmutableMap<String, Object> computeGroovyContext(ReferenceValueRepository referenceValueRepository, GroovyDataInjectionConfiguration groovyDataInjectionConfiguration) {
+        Set<String> configurationReferences = groovyDataInjectionConfiguration.getReferences();
+        ImmutableMap<String, Object> contextForExpression = groovyContextHelper.getGroovyContextForReferences(referenceValueRepository, configurationReferences);
+        Preconditions.checkState(groovyDataInjectionConfiguration.getDatatypes().isEmpty(), "à ce stade, on ne gère pas le chargement de données. Les référentiels ne doivent pas dépendre des données expérimentales.");
+        return contextForExpression;
     }
 
     /**
