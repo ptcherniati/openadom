e68a740df94833956c70bcf395c22fba5f9add86
Brendan Le Ny <bleny@codelutin.com>
2022-04-06 16:44:35 +0200

    JavaDoc sur SomethingThatCanProvideEvaluationContext

--- src/main/java/fr/inra/oresing/model/SomethingThatCanProvideEvaluationContext.java
+++ src/main/java/fr/inra/oresing/model/SomethingThatCanProvideEvaluationContext.java
@@ -2,6 +2,17 @@ package fr.inra.oresing.model;
 
 import com.google.common.collect.ImmutableMap;
 
+/**
+ * Désigne un objet qui a vocation à être exposé dans un contexte Groovy.
+ *
+ * On doit donc pouvoir le transformer dans un objet qui soit exploitable
+ * dans le code Groovy qui va être écrit et donc mettre les chose à plat
+ * avec des objets simples.
+ */
 public interface SomethingThatCanProvideEvaluationContext {
+
+    /**
+     * Récupérer le contenu de cet objet sous forme de Map qui peut être lue en groovy.
+     */
     ImmutableMap<String, Object> getEvaluationContext();
 }
