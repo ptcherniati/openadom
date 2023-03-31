00777ff5a0378ce075fbd8dbb3f869eace594baa
TCHERNIATINSKY <philippe.tcherniatinsky@inrae.fr>
2023-03-31 15:01:08 +0200

    manquait des traductions

--- ui/src/components/common/AuthorizationTableForDatatype.vue
+++ ui/src/components/common/AuthorizationTableForDatatype.vue
@@ -197,12 +197,15 @@ export default {
     },
     selectCheckboxAll(indexColumn) {
       let state = this.getState(indexColumn);
-      let auths =  Object.keys(this.authReferences[0])
-          .filter(path => this.isApplicationAdmin || (this.ownAuthorizationsColumnsByPath[path] || []).includes('admin'));
+      let auths = Object.keys(this.authReferences[0]).filter(
+        (path) =>
+          this.isApplicationAdmin ||
+          (this.ownAuthorizationsColumnsByPath[path] || []).includes("admin")
+      );
       for (const index of auths) {
         if (this.haveRightsOn(index, indexColumn)) {
           let requiredAuthorizations = {};
-          console.log(auths)
+          console.log(auths);
           requiredAuthorizations[this.authReferences[0][auths[index]].authorizationScope] =
             auths[index];
           let authorizations = new Authorization([], requiredAuthorizations);
