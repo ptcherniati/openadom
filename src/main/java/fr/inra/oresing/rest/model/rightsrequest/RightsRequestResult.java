70bfd12b98d6ffefe252fc9b0b6bc9ee6c835c5f
philippe tcheriatinsky <philippe.tcherniatinsky@inrae.fr>
2024-12-16 01:54:05 +0100

    Modification de la demande de droit

--- src/main/java/fr/inra/oresing/rest/model/rightsrequest/RightsRequestResult.java
+++ src/main/java/fr/inra/oresing/rest/model/rightsrequest/RightsRequestResult.java
@@ -5,6 +5,7 @@ import fr.inra.oresing.domain.repository.authorization.OperationType;
 import fr.inra.oresing.rest.model.authorization.AuthorizationParsed;
 import lombok.Value;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -14,7 +15,8 @@ public class RightsRequestResult {
     UUID id;
     UUID application;
     UUID user;
-
+    Timestamp createDate;
+    Timestamp updateDate;
     String comment;
     Map<String, String> rightsRequestForm;
     Map<String, List<AuthorizationParsed>> rightsRequest;
@@ -31,5 +33,7 @@ public class RightsRequestResult {
         rightsRequestForm = rightsRequest.getRightsRequestForm();
         setted = rightsRequest.isSetted();
         this.rightsRequest = authorizationsParsed;
+        this.createDate = Timestamp.valueOf(rightsRequest.getCreationDate());
+        this.updateDate = Timestamp.valueOf(rightsRequest.getUpdateDate());
     }
 }
