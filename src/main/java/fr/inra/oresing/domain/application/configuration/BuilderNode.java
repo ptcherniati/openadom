64d78f99cfb175f73001ae397d97cda3fa4f52f3
philippe tcheriatinsky <philippe.tcherniatinsky@inrae.fr>
2025-02-27 16:00:58 +0100

    correction #14

--- src/main/java/fr/inra/oresing/domain/application/configuration/BuilderNode.java
+++ src/main/java/fr/inra/oresing/domain/application/configuration/BuilderNode.java
@@ -2,28 +2,29 @@ package fr.inra.oresing.domain.application.configuration;
 
 import org.apache.commons.collections4.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public record BuilderNode(
+        int level,
         String nodeName,
         String componentKey,
         String columnToLookUpForRecursive,
         BuilderNode parent,
-        List<String> children,
-        List<String> depends,
+        Set<String> children,
+        Set<String> depends,
         Integer order,
         boolean isRecursive) implements WithDepends {
-    public BuilderNode(final String nodeName, final String componentKey, String columnToLookUpForRecursive, final BuilderNode parent, final List<String> children, final List<String> depends, final Integer order, final boolean isRecursive) {
+    public BuilderNode(final int level, final String nodeName, final String componentKey, String columnToLookUpForRecursive, final BuilderNode parent, final Set<String> children, final Set<String> depends, final Integer order, final boolean isRecursive) {
+        this.level = level;
         this.nodeName = nodeName;
         this.componentKey = componentKey;
         this.columnToLookUpForRecursive = columnToLookUpForRecursive;
         this.parent = parent;
         this.children = children;
-        this.depends = depends.stream().filter(depend->!depend.equals(nodeName)).collect(Collectors.toCollection(ArrayList::new));
+        this.depends = depends.stream().filter(depend->!depend.equals(nodeName)).collect(Collectors.toCollection(TreeSet::new));
         this.order = order;
         this.isRecursive = isRecursive;
     }
@@ -35,25 +36,32 @@ public record BuilderNode(
                 .toList();
     }
 
-    public BuilderNode withAllDepends(final Collection<? extends WithDepends> nodes) {
-        final List<String> depends = depends();
+    protected BuilderNode withAllDepends(final Collection<? extends WithDepends> nodes) {
+        final Set<String> depends = depends();
+        int level = level();
         if (CollectionUtils.isEmpty(depends)) {
             return this;
         }
-        List<String> childDepends = depends();
+        Function<String, ? extends WithDepends> findNodeByName = name -> nodes.stream()
+                    .filter(node -> node.nodeName().equals(name))
+                    .findFirst()
+                    .orElse(null);
+        Set<String> childDepends = depends();
         while (CollectionUtils.isNotEmpty(childDepends)) {
+            level++;
             childDepends = childDepends.stream()
-                    .map(name -> nodes.stream().filter(node -> node.nodeName().equals(name)).findFirst().orElse(null)).filter(Objects::nonNull)
+                    .map(findNodeByName)
+                    .filter(Objects::nonNull)
                     .map(WithDepends::depends)
-                    .flatMap(List::stream)
-                    .toList();
-            depends.addAll(childDepends);
+                    .flatMap(Set::stream)
+                    .collect(Collectors.toSet());
+            childDepends.stream().filter(Predicate.not(childDepends::contains)).forEach(childDepends::add);
         }
-        return new BuilderNode(nodeName(), componentKey(), columnToLookUpForRecursive(), parent(), children(), depends, order(), isRecursive);
+        return new BuilderNode(level, nodeName(), componentKey(), columnToLookUpForRecursive(), parent(), children(), depends, order(), isRecursive);
     }
-
     public BuilderNode withComponentKeyAndRecursive(final String componentKey) {
         return new BuilderNode(
+                level(),
                 nodeName(),
                 componentKey,
                 componentKey,
@@ -67,6 +75,7 @@ public record BuilderNode(
 
     public BuilderNode withComponentKeyAndNotRecursive(final String componentKey, Boolean isParent) {
         return new BuilderNode(
+                level(),
                 nodeName(),
                 isParent?componentKey:componentKey(),
                 columnToLookUpForRecursive(),
@@ -80,6 +89,7 @@ public record BuilderNode(
 
     public BuilderNode withComponentKey(String componentKey) {
         return new BuilderNode(
+                level(),
                 nodeName(),
                 componentKey,
                 columnToLookUpForRecursive(),
