a2968849cbd6602948ed31615f50d0dae059e0a3
philippe tcheriatinsky <philippe.tcherniatinsky@inrae.fr>
2025-03-05 11:00:12 +0100

    Récupération des fichiers d'exemple Correction de la sortie (binaryFileDataset)

--- src/main/java/fr/inra/oresing/rest/model/data/BinaryFileDatasetResult.java
+++ src/main/java/fr/inra/oresing/rest/model/data/BinaryFileDatasetResult.java
@@ -5,16 +5,23 @@ import fr.inra.oresing.domain.application.configuration.Ltree;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record BinaryFileDatasetResult(
         String datatype,
-        Map<String,List<Ltree>> requiredAuthorizations,
+        Map<String, List<String>> requiredAuthorizations,
         String from,
         String to) {
     public static BinaryFileDatasetResult of(BinaryFileDataset binaryFileDataset) {
         return new BinaryFileDatasetResult(
                 binaryFileDataset.getDatatype(),
-                binaryFileDataset.getRequiredAuthorizations(),
+                binaryFileDataset.getRequiredAuthorizations().entrySet()
+                        .stream().collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> entry.getValue().stream()
+                                        .map(Ltree::getSql)
+                                        .toList()
+                        )),
                 binaryFileDataset.getFrom(),
                 binaryFileDataset.getTo()
         );
