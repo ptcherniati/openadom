5c14167e83e99c1fca91569cf156239e0b00d3a8
Philippe TCHERNIATINSKY <philippe.tcherniatinsky@inrae.fr>
2025-10-01 17:02:34 +0200

    Postgresql 18.0 is released

--- src/test/java/fr/inra/oresing/TestDatabaseConfig.java
+++ src/test/java/fr/inra/oresing/TestDatabaseConfig.java
@@ -14,7 +14,7 @@ public class TestDatabaseConfig {
     @Bean
     @Primary
     public DataSource dataSource() {
-        PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18rc1")
+        PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18.0")
                 .withDatabaseName("test")
                 .withUsername("postgres")
                 .withPassword("postgres")
